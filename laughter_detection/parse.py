f1abb7f2173354eb443f87b9ebda7d550c494f2d
Lasse Wolter <wolter.lasse@gmail.com>
2021-10-10 16:28:32 +0100

    Improved and extended python parsing script
    + parses data to a pandas dataframe
    + can parse .mrt as well as .TextGrid files
    + added a docoment for notes that came up during script development

--- laughter_detection/parse.py
+++ laughter_detection/parse.py
@@ -1,66 +1,79 @@
 from lxml import etree
-# Using lxml instead of xml.etree.ElementTree because it has full XPath support 
+# Using lxml instead of xml.etree.ElementTree because it has full XPath support
 # xml.etree.ElementTree only supports basic XPath syntax
 import os
+import pandas as pd
+import textgrids
 
 xpath_exp = "//Segment[VocalSound[contains(@Description,'laugh')][preceding-sibling::text() \
         [normalize-space()=''] and following-sibling::text()[normalize-space()='']] and count(./*) < 2]"
 
 
+def parse_xml_to_list(xml_seg):
+    part_id = xml_seg.get('Participant')
+    start = float(xml_seg.get('StartTime'))
+    end = float(xml_seg.get('EndTime'))
+    length = end-start
+    # [0] is the first child tag which is guaranteed to be a VocalSound
+    # due to the XPath expression used for parsing the XML document
+    l_type = xml_seg[0].get('Description')
+    return [part_id, start, end, length, l_type]
 
-class Segment:
-    def __init__(self, start, end,l_type ):
-        self.start_time = float(start)
-        self.end_time = float(end)
-        self.laugh_type= l_type
-        self.seg_len = self.end_time - self.start_time 
 
-
-    @classmethod
-    def from_xml_segment(cls, xml_seg):
-        """
-        Takes an etree element representing a Segment tag in XML
-        """
-        start = xml_seg.get('StartTime')
-        end = xml_seg.get('EndTime')
-        # [0] is the first child tag which is guaranteed to be a VocalSound
-        # due to the XPath expression used for parsing the XML document
-        l_type = xml_seg[0].get('Description')
-        return cls(start, end, l_type)
-
-    def __repr__(self):
-        return "{} - {} \t [{:.2f}] => Type: {}".format(self.start_time, self.end_time, \
-                self.seg_len, self.laugh_type)
-
-    def __str__(self):
-        return "{} - {} \t [{:.2f}] => Type: {}".format(self.start_time, self.end_time, \
-                self.seg_len, self.laugh_type)
-
-def get_laugh_segments(filename):
-    print(filename)
-    segs_list = [] 
+def laughs_to_list(filename, meeting_id):
+    seg_list = []
     tree = etree.parse(filename)
     laugh_segs = tree.xpath(xpath_exp)
+
     for seg in laugh_segs:
-        new_seg = Segment.from_xml_segment(seg)
-        segs_list.append(new_seg)
-    return segs_list
+        seg_list.append([meeting_id] + parse_xml_to_list(seg))
+    return seg_list
 
 
-def main():
+def laughs_to_df():
     tot_laugh_segs = []
     for filename in os.listdir('data'):
-        if filename.endswith('.mrt'):
-            tot_laugh_segs += get_laugh_segments(os.path.join('data',filename))
+        if filename.endswith('Bed015.mrt'):
+            # First split for cutting of .mrt
+            # second split for discarding parent dirs
+            meeting_id=filename.split('.')[0].split('/')[-1]
+            full_path = os.path.join('data', filename)
+            tot_laugh_segs += laughs_to_list(full_path, meeting_id)
 
-    print(len(tot_laugh_segs))
-    avg_seg_len = 0
+    cols = ['Meeting', 'ID', 'Start', 'End', 'Length', 'Type']
+    df = pd.DataFrame(tot_laugh_segs, columns=cols)
+    print(df[df["ID"]=="mn015"])
+    print('avg-snippet-length: {} '.format(df['Length'].mean()))
 
-    for seg in tot_laugh_segs:
-        avg_seg_len += (seg.end_time-seg.start_time)/len(tot_laugh_segs)
 
-    print(avg_seg_len)
+def textgrid_to_list(filename, meeting_id):
+    interval_list = []
+    grid = textgrids.TextGrid(filename)
+    for interval in grid['laughter']:
+        # TODO: Change for belly laugh?!
+        if str(interval.text) == 'laugh':
+            interval_list.append([meeting_id, 'xxx', interval.xmin, 
+                interval.xmax, interval.xmax-interval.xmin, str(interval.text)])
+    return interval_list
 
-    
+def textgrid_to_df():
+    tot_list=[]
+    for filename in os.listdir('data'):
+        if filename.endswith('chan0_laughter.TextGrid'):
+            full_path = os.path.join('data', filename)
+            # First split for cutting of .mrt
+            # second split for discarding parent dirs
+            meeting_id=filename.split('.')[0].split('/')[-1]
+            tot_list += textgrid_to_list(full_path, meeting_id)
+
+    cols = ['Meeting', 'ID', 'Start', 'End', 'Length', 'Type']
+    df = pd.DataFrame(tot_list, columns=cols)
+    print(df)
+
+def main():
+    textgrid_to_df()
+    laughs_to_df()
+
+
 if __name__ == "__main__":
     main()
