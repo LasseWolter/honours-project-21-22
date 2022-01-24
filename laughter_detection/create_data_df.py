bfd1e37861cf0a7916de960ba5e56ec1ac4ffd61
Lasse Wolter <wolter.lasse@gmail.com>
2022-01-24 12:06:48 +0000

    [code] Updated create_data_df to have fixed splits
    + splits are set by meeting and then audio samples are taken according
      to that
      -> before all audio samples were split in the end which doesn't allow
      for controlling speaker overlap

--- laughter_detection/create_data_df.py
+++ laughter_detection/create_data_df.py
@@ -9,18 +9,42 @@ import os
 import subprocess
 
 
-def get_random_speech_segment(duration):
+# Taken from lhotse icsi recipe to minimise speaker overlap
+PARTITIONS = {
+    'train': [
+        "Bdb001", "Bed002", "Bed003", "Bed004", "Bed005", "Bed006", "Bed008", "Bed009",
+        "Bed010", "Bed011", "Bed012", "Bed013", "Bed014", "Bed015", "Bed016", "Bed017",
+        "Bmr001", "Bmr002", "Bmr003", "Bmr005", "Bmr006", "Bmr007", "Bmr008", "Bmr009",
+        "Bmr010", "Bmr011", "Bmr012", "Bmr014", "Bmr015", "Bmr016", "Bmr019", "Bmr020",
+        "Bmr022", "Bmr023", "Bmr024", "Bmr025", "Bmr026", "Bmr027", "Bmr028", "Bmr029",
+        "Bmr030", "Bmr031", "Bns002", "Bns003", "Bro003", "Bro004", "Bro005", "Bro007",
+        "Bro008", "Bro010", "Bro011", "Bro012", "Bro013", "Bro014", "Bro015", "Bro016",
+        "Bro017", "Bro018", "Bro019", "Bro022", "Bro023", "Bro024", "Bro025", "Bro026",
+        "Bro027", "Bro028", "Bsr001", "Btr001", "Btr002", "Buw001",
+    ],
+    'val': ["Bmr021", "Bns001"],
+    'test': ["Bmr013", "Bmr018", "Bro021"]
+}
+
+
+def get_random_speech_segment(duration, meeting_id):
+    '''
+    Get a random speech segment from any channel in the passed meeting
+    If there is an overlap between this segment and laughter/invalid regions, resample
+    '''
+    # Only consider segments with passed meeting_id
+    info_df = parse.info_df[parse.info_df.meeting_id == meeting_id]
     # Get segment info for this segment from info_df
-    row_number = parse.info_df.shape[0]
-    row_ind = np.random.randint(0, row_number)
-    sample_seg = parse.info_df.iloc[row_ind]
+    num_of_rows = info_df.shape[0]
+    row_ind = np.random.randint(0, num_of_rows)
+    sample_seg = info_df.iloc[row_ind]
     start = np.random.uniform(0, sample_seg.length-duration)
     speech_seg = P.closed(utils.to_frames(
         start), utils.to_frames(start+duration))
 
-    # If segment overlaps with any laughter or invalid segment resample
+    # If segment overlaps with any laughter or invalid segment, resample
     if (utils.seg_overlaps(speech_seg, [prep.laugh_index, prep.invalid_index], sample_seg.meeting_id, sample_seg.part_id)):
-        return get_random_speech_segment(duration)
+        return get_random_speech_segment(duration, meeting_id)
     else:
         sub_start, sub_duration = get_subsample(
             start, duration, cfg.train['subsample_duration'])
@@ -36,74 +60,65 @@ def get_subsample(start, duration, subsample_duration):
     return subsample_start, subsample_duration
 
 
-def train_val_test_split(df, fracs):
-    '''
-    Split pd.Dataframe into 3 dataframes of given fractions [train, validation]. The test set takes the remainder fraction.
-    '''
-    train = df.sample(frac=fracs[0])
-    remain_df = df.drop(index=train.index)
-    # Recalculate fractions for remaining_df
-    val_frac = 0.1 / (1-fracs[0])
-    val = remain_df.sample(frac=val_frac)
-    test = remain_df.drop(index=val.index)
-    return train, val, test
-
-
 def create_data_df(data_dir):
     '''
-    Create a dataframe with training data exactly structured like in the model by Gillick et al.
+    Create 3 dataframes (train,val,test) with data exactly structured like in the model by Gillick et al.
     Columns:
         [region start, region duration, subsampled region start, subsampled region duration, audio path, label]
-
-    Also creates 3 small splits for debuggin (all .csv files preceded with 'small_')
 
     Subsampled region are sampled once during creation. Later either the sampled values can be used or resampling can happen.
     (see Gillick et al. for more details)
     Duration of the subsamples is defined in config.py
     '''
     np.random.seed(cfg.train['random_seed'])
-    speech_seg_list = []
-    laugh_seg_list = []
+    speech_seg_lists = {'train': [], 'val': [], 'test': []}
+    laugh_seg_lists = {'train': [], 'val': [], 'test': []}
 
-    # For each laughter segment get a random speech segment with the same length
-    for _, laugh_seg in parse.laugh_only_df.iterrows():
-        # Get and append random speech segment of same length as current laugh segment
-        speech_seg_list.append(get_random_speech_segment(laugh_seg.length))
+    meeting_groups = parse.laugh_only_df.groupby('meeting_id')
 
-        # Subsample laugh segment and append to list
-        audio_path = os.path.join(
-            laugh_seg.meeting_id, f'{laugh_seg.chan}.sph')
-        sub_start, sub_duration = get_subsample(
-            laugh_seg.start, laugh_seg.length, cfg.train['subsample_duration'])
+    for meeting_id, meeting_laugh_df in meeting_groups:
+        split = 'train'
+        if meeting_id in PARTITIONS['val']:
+            split = 'val'
+        elif meeting_id in PARTITIONS['test']:
+            split = 'test'
 
-        laugh_seg_list.append(
-            [laugh_seg.start, laugh_seg.length, sub_start, sub_duration, audio_path, 1])
+        # For each laughter segment get a random speech segment with the same length
+        for _, laugh_seg in meeting_laugh_df.iterrows():
+            # Get and append random speech segment of same length as current laugh segment
+            speech_seg_lists[split].append(
+                get_random_speech_segment(laugh_seg.length, meeting_id))
 
+            # Subsample laugh segment and append to list
+            audio_path = os.path.join(
+                laugh_seg.meeting_id, f'{laugh_seg.chan}.sph')
+            sub_start, sub_duration = get_subsample(
+                laugh_seg.start, laugh_seg.length, cfg.train['subsample_duration'])
+
+            laugh_seg_lists[split].append(
+                [laugh_seg.start, laugh_seg.length, sub_start, sub_duration, audio_path, 1])
+
+    # Columns for data_dfs - same for speech and laughter as they will be combined to one df
     cols = ['start', 'duration', 'sub_start',
             'sub_duration', 'audio_path', 'label']
-    speech_df = pd.DataFrame(speech_seg_list, columns=cols)
-    laugh_df = pd.DataFrame(laugh_seg_list, columns=cols)
-    whole_df = pd.concat([speech_df, laugh_df], ignore_index=True)
 
-    # Round all floats to certain number of decimals (defined in config)
-    whole_df = whole_df.round(cfg.train['float_decimals'])
-    small_df = whole_df[whole_df.audio_path.str.contains('Bdb001')]
-
+    # Create output directory for dataframes
     subprocess.run(['mkdir', '-p', data_dir])
-    train_df, val_df, test_df = train_val_test_split(
-        whole_df, cfg.train['train_val_test_split'])
-    train_df.to_csv(os.path.join(data_dir, 'train_df.csv'))
-    val_df.to_csv(os.path.join(data_dir, 'val_df.csv'))
-    test_df.to_csv(os.path.join(data_dir, 'test_df.csv'))
 
-    # Create small split for debugging
-    small_train_df, small_val_df, small_test_df = train_val_test_split(
-        small_df, cfg.train['train_val_test_split'])
-    small_train_df.to_csv(os.path.join(data_dir, 'small_train_df.csv'))
-    small_val_df.to_csv(os.path.join(data_dir, 'small_val_df.csv'))
-    small_test_df.to_csv(os.path.join(data_dir, 'small_test_df.csv'))
+    for split in PARTITIONS.keys():  # [train,val,test]
+        speech_df = pd.DataFrame(speech_seg_lists[split], columns=cols)
+        laugh_df = pd.DataFrame(laugh_seg_lists[split], columns=cols)
+        whole_df = pd.concat([speech_df, laugh_df], ignore_index=True)
+        # Round all floats to certain number of decimals (defined in config)
+        whole_df = whole_df.round(cfg.train['float_decimals'])
+        whole_df.to_csv(os.path.join(data_dir, f'{split}_df.csv'))
 
-    print(whole_df)
+        # Check that df only contains correct meeting ids for this split
+        audio_paths = whole_df.audio_path.unique().tolist()
+        meeting_ids = set(map(lambda x: x.split('/')[0], audio_paths))
+        mismatched_meetings = meeting_ids - set(PARTITIONS[split])
+        assert len(
+            mismatched_meetings) == 0, f"Found meetings in {split}_df with meeting_id not corresponding to that split, namely: {mismatched_meetings}"
 
 
 if __name__ == "__main__":
