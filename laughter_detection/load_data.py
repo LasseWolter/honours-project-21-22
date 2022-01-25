138859a7bf4c1b56e9a543341f308bc8cdc09dbf
Lasse Wolter <wolter.lasse@gmail.com>
2022-01-25 02:18:46 +0000

    [code] STILL IN PROGRESS Updated load_data script
    - using LilcomFilesWriter to store each matrix in a single file

--- laughter_detection/load_data.py
+++ laughter_detection/load_data.py
@@ -1,5 +1,5 @@
 from torch.utils.data import DataLoader
-from lhotse import CutSet, Fbank, MonoCut
+from lhotse import CutSet, Fbank, MonoCut, LilcomFilesWriter
 from lhotse.dataset import VadDataset, SingleCutSampler
 from lhotse.recipes import prepare_icsi
 import pandas as pd
@@ -9,45 +9,45 @@ import pandas as pd
 # the sampling rate, number of channels, duration, etc.
 # The SupervisionSet describes metadata about supervision segments:
 # the transcript, speaker, language, and so on.
-icsi, chan_idx_map = prepare_icsi(audio_dir='data/speech/test', transcripts_dir='data/transcripts/test', output_dir='lhotse_manifests')
+icsi, chan_idx_map = prepare_icsi(
+    audio_dir='data/speech/test', transcripts_dir='data/transcripts/test', output_dir='lhotse_manifests')
 
 
 # Read data_dfs containing the samples for train,val,test split
 # train_df = pd.read_csv('data_dfs/train_df.csv')
-# val_df = pd.read_csv('data_dfs/val_df.csv') 
-# test_df = pd.read_csv('data_dfs/test_df.csv') 
+# val_df = pd.read_csv('data_dfs/val_df.csv')
+# test_df = pd.read_csv('data_dfs/test_df.csv')
 
 dummy_df = pd.read_csv('data_dfs/dummy_df.csv')
 
 
-
 # CutSet is the workhorse of Lhotse, allowing for flexible data manipulation.
 # We create 5-second cuts by traversing ICSI recordings in windows.
-# No audio data is actually loaded into memory or stored to disk at this point.  
+# No audio data is actually loaded into memory or stored to disk at this point.
 # Columns of dataframes look like this:
 #   cols = ['start', 'duration', 'sub_start', 'sub_duration', 'audio_path', 'label']
 cut_list = []
 for ind, row in dummy_df.iterrows():
     meeting_id = row.audio_path.split('/')[0]
     channel = row.audio_path.split('/')[1].split('.')[0]
-
     chan_id = chan_idx_map[meeting_id][channel]
-
-    # Just for now, because not all channels have been downloaded -> mistake in prepare_icsi() 
-    if chan_id >6: continue
+    # Just for now, because not all channels have been downloaded -> mistake in prepare_icsi()
+    if chan_id > 6:
+        continue
     # Dummy recording is in the train split of the lhotse manifest
-    cut = MonoCut(id=f'dummy_{ind}', start = row.start, duration=row.duration,
-        recording=icsi['train']['recordings'][meeting_id], channel=chan_id
-    )
+    cut = MonoCut(id=f'dummy_{ind}', start=row.start, duration=row.duration,
+                  recording=icsi['train']['recordings'][meeting_id], channel=chan_id)
     cut_list.append(cut)
 
-cutset = CutSet.from_cuts(cut_list) 
+cutset = CutSet.from_cuts(cut_list)
 
 start_set = cutset.subset(first=10)
 
-cuts = start_set.compute_and_store_features(
+cuts = cutset.compute_and_store_features(
     extractor=Fbank(),
-    storage_path='test'
+    storage_path='feats',
+    num_jobs=8,
+    storage_type=LilcomFilesWriter
 )
 
 # Construct a Pytorch Dataset class for Voice Activity Detection task:
